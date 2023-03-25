affd958a3d9e83da97925993ae8bf52e1a300a5f
illiakalinin <illinoiz4305@gmail.com>
2023-03-25 19:26:13 +0200

    Add user create controller

---
+++ models/phone.js
@@ -0,0 +1,9 @@
+class Phone {
+  static create () {}
+  static getAll () {}
+  static getById () {}
+  static updateById () {}
+  static deleteByid () {}
+}
+
+module.exports = Phone;
---
+++ models/user.js
@@ -0,0 +1,26 @@
+class User {
+  static async create ({ firstName, lastName, email, tel }) {
+    //прописати sql запит
+
+    const insertQuery = `
+    INSERT INTO users (first_name, last_name, email, tel)
+    VALUES (${firstName}, ${lastName}, ${email}, ${tel})
+    RETURNING *;
+    `;
+    try {
+      // виконати запит
+      const createdUser = await User.pool.query(insertQuery);
+      console.log('createdUser :>> ', createdUser.rows[0]);
+      return createdUser;
+    } catch (err) {
+      //повернути результат
+      console.log('err :>> ', err);
+    }
+  }
+  static getAll () {}
+  static getById () {}
+  static updateById () {}
+  static deleteByid () {}
+}
+
+module.exports = User;
