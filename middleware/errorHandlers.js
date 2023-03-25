3f92806c59fc86e7c37fdaf6ab5c45047d579e3b
illiakalinin <illinoiz4305@gmail.com>
2023-03-25 18:26:19 +0200

    Add errorHandlers

---
+++ middleware/errorHandlers.js
@@ -0,0 +1,9 @@
+module.exports.errorHandler = (err, req, res, next) => {
+  if (res.headersSent) {
+    return;
+  }
+
+  res
+    .status(err.status ?? 500)
+    .send({ message: err.message ?? 'Server Error' });
+};
