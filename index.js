cbe8c59fa5fad0659e888bba5a601ea150e57914
illiakalinin <illinoiz4305@gmail.com>
2023-03-30 20:47:40 +0300

    Add server

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const PORT = process.env.PORT || 5000;
 
 const httpServer = http.createServer(app);
 
-httpServer.listen(PORT, () =>
-  console.log(`Server is listening http://localhost:${PORT}`)
-);
+httpServer.listen(PORT, () => {
+  console.log(`Server is listening http://localhost:${PORT}`);
+});
