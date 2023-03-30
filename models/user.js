1430b42657acbf78b7c3e15b1613a053351172e9
illiakalinin <illinoiz4305@gmail.com>
2023-03-30 20:45:57 +0300

    Add models

--- models/user.js
+++ models/user.js
@@ -4,23 +4,77 @@ class User {
 
     const insertQuery = `
     INSERT INTO users (first_name, last_name, email, tel)
-    VALUES (${firstName}, ${lastName}, ${email}, ${tel})
+    VALUES ('${firstName}', '${lastName}', '${email}', '${tel}')
     RETURNING *;
     `;
     try {
       // виконати запит
       const createdUser = await User.pool.query(insertQuery);
-      console.log('createdUser :>> ', createdUser.rows[0]);
-      return createdUser;
+      return createdUser.rows[0];
     } catch (err) {
       //повернути результат
-      console.log('err :>> ', err);
+      throw new Error(err.detail);
     }
   }
-  static getAll () {}
+
+  static async getAll ({ limit, offset }) {
+    const selectQuery = `
+    SELECT *
+    FROM users
+    ORDER BY id
+    LIMIT ${limit} OFFSET ${offset};
+    `;
+    try {
+      const foundUsers = await User.pool.query(selectQuery);
+      return foundUsers.rows;
+    } catch (err) {
+      throw new Error(err.detail);
+    }
+  }
+
   static getById () {}
-  static updateById () {}
-  static deleteByid () {}
+
+  // SELECT brand, model
+  // FROM users_to_phones
+  // WHERE u_id IN (
+  //   SELECT u_id
+  //   FROM users_to_phones
+  //   WHERE id = ${id}
+  // );
+
+  static async updateById (id, body) {
+    const { email, tel } = body;
+
+    const updateQuery = `
+    UPDATE users
+    SET email = '${email}',
+        tel = '${tel}'
+    WHERE id = ${id}
+    RETURNING *;
+    `;
+    try {
+      const updatedUser = await User.pool.query(updateQuery);
+      console.log('updatedUser :>> ', updatedUser);
+      return updatedUser.rows[0];
+    } catch (err) {
+      throw new Error(err.detail);
+    }
+  }
+
+  static async deleteById (id) {
+    const deleteQuery = `
+    DELETE FROM users
+    WHERE id = ${id}
+    RETURNING id;
+    `;
+    try {
+      const deletedUser = await User.pool.query(deleteQuery);
+      console.log('deletedUser :>> ', deletedUser);
+      return deletedUser.rows[0];
+    } catch (err) {
+      throw new Error(err.detail);
+    }
+  }
 }
 
 module.exports = User;
